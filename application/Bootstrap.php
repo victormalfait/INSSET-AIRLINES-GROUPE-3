85e5fe7882056f6eca2ff0d7c3aee9455bd3243d
anthony <anthony@anthony-GE70-0NC-GE70-0ND>
2012-10-22 17:39:16 +0200

    installation du poste

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,10 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
-
+	public function run()
+	{
+		Zend_Registry::set('config', new Zend_Config($this->getOptions()));
+		parent::run();
+	}
 }
 
