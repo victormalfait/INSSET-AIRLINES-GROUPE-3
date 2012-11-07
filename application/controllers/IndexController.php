f13d91a9ac3b914d728e71ec52fa537fbce91fce
anthony <anthony@anthony-GE70-0NC-GE70-0ND>
2012-11-07 21:10:02 +0100

    Test
    Signed-off-by: anthony <anthony@anthony-GE70-0NC-GE70-0ND>

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,26 +5,12 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-
+    	
     }
 
     public function indexAction()
     {
-        $monform = new Zend_Form;
-		
-		//paramétres du formulaire
-		$monform->setMethod('post');
-		$monform->setAction('/index/#');
-		$monform->setAttrib('id', 'monformulaire');
-		
-		//creation élément formulaire
-		$ePilot = new Zend_Form_Element_Select('listpilot');
-		$ePilot->setLabel('Liste des pilotes : ');
-		$ePilot->setMultiOptions(array('1' => 'Jean Charle', '2' => 'Amstrong Frederic'));
-		
-		$monform->addElement($ePilot);
-		
-		echo $monform;
+    	
     }
 
 
