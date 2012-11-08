5f12b4916c01dea0484e8a9c73ce556e3d33bf0b
arnaud-23 <arnaud.h.lefevre@gmail.com>
2012-11-08 16:52:08 +0100

    finalisation css connexion

--- application/forms/FConnexion.php
+++ application/forms/FConnexion.php
@@ -15,32 +15,52 @@ class FConnexion extends Zend_Form
 		$this->clearDecorators();
 
 		// Creer les decorateurs
-		$decorators = new InssetAirline_Form_Decorator_Connexion();
-		$decorators = array($decorators);
+		// $decorators = new InssetAirline_Form_Decorator_Connexion();
+		// $decorators = array($decorators);
+
+		$decorators = array(
+		    'ViewHelper',
+		    'Errors',
+		    array('Label', array('class' => 'label')),
+		    array('HtmlTag', array('tag' => 'li'))
+		);
+
+		$decoratorsForm = array(
+			'FormElements',
+			array('HtmlTag', array('tag' => 'ul')),
+			array(
+				array('DivTag' => 'HtmlTag'),
+				array('tag' => 'div')
+			),
+			'Form'
+		);
 
 		$eEmail = new Zend_Form_Element_Text('email');
 		$eEmail		->setLabel('Identifiant')
 		        	->setRequired(true)
 		            ->addFilter('StripTags')
-		            ->addFilter('StringTrim');
+		            ->addFilter('StringTrim')
+		            ->setDecorators($decorators);
 
 
 		$ePass = new Zend_Form_Element_Password('password');
 		$ePass 		->setLabel('Mot de pase')
 					->setRequired(true)
 					->addFilter('StripTags')
-					->addFilter('StringTrim');
+					->addFilter('StringTrim')
+					->setDecorators($decorators);
 
 		$eSubmit = new Zend_Form_Element_Submit('submit');
 		$eSubmit 	->setAttrib('id', 'submitbutton')
-					->setLabel('Se connecter');
+					->setLabel('Se connecter')
+					->setDecorators($decorators);
 
 		$elements = array($eEmail,$ePass, $eSubmit);
 		$this->addElements($elements);
 
 		$this->setDecorators(array(
 		'FormElements',
-		array('HtmlTag', array('tag' => 'dl', 'class' => 'zend_form')),
+		array('HtmlTag', array('tag' => 'ul', 'class' => 'zend_form')),
 		array('Errors', array('placement' => 'apend')),
 		'Form'
 		));
