15e972f14f236020009a7851ea67683accefbe61
arnaud-23 <arnaud.h.lefevre@gmail.com>
2012-11-09 09:22:03 +0100

    integration responsive web design

--- application/controllers/StrategieController.php
+++ application/controllers/StrategieController.php
@@ -5,7 +5,7 @@ class StrategieController extends Zend_Controller_Action
 
     public function indexAction()
     {
-    // 	// creation de l'objet formulaire
+    //  // creation de l'objet formulaire
     //     $form = new FNouveauVol;
 
     //     // affichage du formulaire
@@ -14,26 +14,26 @@ class StrategieController extends Zend_Controller_Action
     //     // traitement du formulaire
     //     // si le formulaire a été soumis
     //     if ($this->_request->isPost()) {
-    //     	// on recupere les éléments
+    //      // on recupere les éléments
     //         $formData = $this->_request->getPost();
 
     //         // si le formulaire passe au controle des validateurs
     //         if ($form->isValid($formData)) {
 
-    //         	//on envoi la requete
+    //          //on envoi la requete
     //             $destination = new TDestination;
     //             $row = $destination->createRow();
     //             $row->tri_aero_dep = $form->getValue('aeroportDepart');
     //             $row->heure_dep = $form->getValue('departH') . 'h' . $form->getValue('departM');
     //             $row->tri_aero_arr = $form->getValue('aeroportArrivee');
-				// $row->heure_arr = $form->getValue('arriveeH') . 'h' . $form->getValue('arriveeM');
+                // $row->heure_arr = $form->getValue('arriveeH') . 'h' . $form->getValue('arriveeM');
     //             $row->periodicite = $form->getValue('periodicite');
     //             $row->date_dep = $form->getValue('dateDep');
 
     //             //sauvegarde de la requete
     //             $result = $row->save();
-		
-				// // RAZ du formulaire
+        
+                // // RAZ du formulaire
     //             $form->reset();
     //         }
     //     }
@@ -41,7 +41,7 @@ class StrategieController extends Zend_Controller_Action
 
     public function nouveauvolAction()
     {
-    	// creation de l'objet formulaire
+        // creation de l'objet formulaire
         $form = new FNouveauVol;
 
         // affichage du formulaire
@@ -50,26 +50,26 @@ class StrategieController extends Zend_Controller_Action
         // traitement du formulaire
         // si le formulaire a été soumis
         if ($this->_request->isPost()) {
-        	// on recupere les éléments
+            // on recupere les éléments
             $formData = $this->_request->getPost();
 
             // si le formulaire passe au controle des validateurs
             if ($form->isValid($formData)) {
 
-            	//on envoi la requete
+                //on envoi la requete
                 $destination = new TDestination;
                 $row = $destination->createRow();
                 $row->tri_aero_dep = $form->getValue('aeroportDepart');
                 $row->heure_dep = $form->getValue('departH') . 'h' . $form->getValue('departM');
                 $row->tri_aero_arr = $form->getValue('aeroportArrivee');
-				$row->heure_arr = $form->getValue('arriveeH') . 'h' . $form->getValue('arriveeM');
+                $row->heure_arr = $form->getValue('arriveeH') . 'h' . $form->getValue('arriveeM');
                 $row->periodicite = $form->getValue('periodicite');
                 $row->date_dep = $form->getValue('dateDep');
 
                 //sauvegarde de la requete
                 $result = $row->save();
-		
-				// RAZ du formulaire
+        
+                // RAZ du formulaire
                 $form->reset();
             }
         }
