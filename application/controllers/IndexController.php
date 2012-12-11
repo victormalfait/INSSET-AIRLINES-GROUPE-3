80e34d7850b763d97f7c9de5cd47e84403860e84
arnaud-23 <arnaud.h.lefevre@gmail.com>
2012-12-11 15:56:53 +0100

    index controller

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,10 +10,7 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-    	$redirector = $this->_helper->getHelper('Redirector');
-        $redirector->gotoUrl("connexion/index");
+
     }
-
-
 }
 
