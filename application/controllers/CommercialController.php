436456b5f53c5c828f4aa769d70e5a58a1898bd9
arnaud-23 <arnaud.h.lefevre@gmail.com>
2012-12-11 12:07:22 +0100

    partie commercial

--- application/controllers/CommercialController.php
+++ application/controllers/CommercialController.php
@@ -2,9 +2,19 @@
 
 class CommercialController extends Zend_Controller_Action
 {
+
 	public function indexAction()
     {
+    	// on charge le formulaire FCommercial
     	$formCommercial = new FCommercial;
+
+    	// on l'envoi à la vu
     	$this->view->formCommercial = $formCommercial;
     }
+
+    public function catalogueAction ()
+    {
+    	
+    }
+
 }
