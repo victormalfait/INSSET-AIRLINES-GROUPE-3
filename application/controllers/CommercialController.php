a9635a4b17f90a426a2822e515dc016f235a9983
victormalfait <victor.malfait@gmail.com>
2013-01-13 22:05:13 +0100

    commercial debut de traitement de la requete

--- application/controllers/CommercialController.php
+++ application/controllers/CommercialController.php
@@ -12,43 +12,64 @@ class CommercialController extends Zend_Controller_Action
     	$this->view->formCommercial = $formCommercial;
 
         if ($this->_request->isPost()) {
-                // on recupere les éléments
-                $formData = $this->_request->getPost();
+            // on recupere les éléments
+            $formData = $this->_request->getPost();
 
-                // si le formulaire passe au controle des validateurs
-                if ($form->isValid($formData)) {
+            // si le formulaire passe au controle des validateurs
+            if ($formCommercial->isValid($formData)) {
 
-                    //on envoi la requete
-                    $tableVols = new TVols;
+                $date_debut = $formCommercial->getValue('datepickerdeb');
 
-                    $date_debut = $formCommercial->getValue('datepickerdeb');
-                    $date_fin = $formCommercial->getValue('datepickerfin');
-                    list($jourD, $moisD, $anneeD) = explode("-", $date_debut);
-                    list($jourF, $moisF, $anneeF) = explode("-", $date_fin);           
+                list($jourD, $moisD, $anneeD) = explode("-", $date_debut);          
 
-                    $date_depart = mktime(0, 0, 0,  $moisD, $jourD, $anneeD);
-                    $date_fin = mktime(0, 0, 0, $moisF, $jourF, $anneeF); 
+                $date_depart = mktime(0, 0, 0,  $moisD, $jourD, $anneeD);
 
-                    $volRequet = $tableVols->select()->where()
-                    $vols = $tableVols->fetchAll();
+                $date_depart_avant = $date_depart - 3*7*24*60*60;
+                $date_depart_apres = $date_depart + 3*7*24*60*60;
 
-                    $tableDestination = new TDestination;
-                    $table_content = array();
-                    $count = 0;
+                //on envoi la requete
+                $tableAeroport = new TAeroport;
 
-                    foreach ($vols as $value) {
-                        $destiantionRequest = $tableDestination->select()->from()
-                                                                         ->where('id_destination ='.$value['id_destination'])
-                                                                         ->where('date_dep >='.$date_depart.'or periodicite = '.$jour_depart);
-                        $destination = $tableDestination->fetchAll($destiantionRequest); 
-                        $table_content[$count]['etat'] = $event->etat_tache;
-                         
+                $aeroportDepartRequest = $tableAeroport->select()->where('id_ville = ?', $formCommercial->getValue('aeroportDepart'));
+                $aeroportArrivetRequest = $tableAeroport->select()->where('id_ville = ?', $formCommercial->getValue('aeroportArrive'));
+
+                $aeroportDeparts = $tableAeroport->fetchAll($aeroportDepartRequest);
+                $aeroportArrives = $tableAeroport->fetchAll($aeroportArrivetRequest);
+
+                $table_content = array();
+                $count = 0;
+
+                foreach ($aeroportDeparts as $aeroportDepart) {
+                    foreach ($aeroportArrives as $aeroportArrive) {
+                        $tableDestination = new TDestination;
+                        $destinationRequest = $tableDestination->select()->where('tri_aero_dep = ?', $aeroportDepart->trigramme)->where('tri_aero_arr = ?', $aeroportArrive->trigramme)->where('plannification = ?', 1);
+
+                        $destinations = $tableDestination->fetchAll($destinationRequest);
+
+                        foreach ($destinations as $destination) {
+                            $tableVols = new TVols;
+                            $volRequest = $tableVols->select()->where('id_destination = ?', $destination->id_destination);
+                            $vols = $tableVols->fetchAll($volRequest);
+                            foreach ($vols as $vol) {
+                                if($vol->heure_dep >= $date_depart_avant && $vol->heure_dep <= $date_depart_apres){
+                                    $table_content[$count]['numero_vol'] = $destination->numero_vol;
+                                    $table_content[$count]['heure_dep'] = $vol->heure_dep;
+                                    $table_content[$count]['heure_arr'] = $vol->heure_arr;
+                                    $table_content[$count]['depart'] = $aeroportDepart->nom_aeroport;
+                                    $table_content[$count]['arrive'] = $aeroportArrive->nom_aeroport;
+                                    $count++;
+                                }
+                            }
+                        }   
                     }
-                                                                 
-                    // RAZ du formulaire
-                    $formCommercial->reset();
-                }
+                }                
+
+                $this->view->tabVol = $table_content;
+                                                                          
+                // RAZ du formulaire
+                $formCommercial->reset();
             }
+        }
     }
 
     public function catalogueAction ()
