b89d644c86e1868f00398d4f8a14d5312458b326
arnaud-23 <arnaud.h.lefevre@gmail.com>
2013-01-30 12:08:24 +0100

    integration reservation

--- application/forms/FCommercial.php
+++ application/forms/FCommercial.php
@@ -2,201 +2,252 @@
 
 class FCommercial extends Zend_Form
 {
-	public function init()
-	{
-	
-	//===============Parametre du formulaire
-		$this->setMethod('post');
-		$this->setAction('/commercial/recherche');
-		$this->setAttrib('id', 'FCommercial');
+    public function init()
+    {
+    
+    //===============Parametre du formulaire
+        $this->setMethod('post');
+        $this->setAction('/commercial/recherche');
+        $this->setAttrib('id', 'FCommercial');
 
-	//=============== creation des decorateurs
-		// Descativation des decorateurs par defaut
-		$this->clearDecorators();
+    //=============== creation des decorateurs
+        // Descativation des decorateurs par defaut
+        $this->clearDecorators();
 
-		//decorateur d'element
-		$decorators = array(
-		    'ViewHelper',
-		    'Errors',
-		    array('Label', array('class' => 'label')),
-		    array('HtmlTag', array('tag' => 'li'))
-		);
+        //decorateur d'element
+        $decorators = array(
+            'ViewHelper',
+            'Errors',
+            array('Label', array('class' => 'label')),
+            array('HtmlTag', array('tag' => 'li'))
+        );
 
-		//decorateur de formulaire
-		$decoratorsForm = array(
-			'FormElements',
-			array('HtmlTag', array('tag' => 'ul')),
-			array(
-				array('DivTag' => 'HtmlTag'),
-				array('tag' => 'div')
-			),
-			'Form'
-		);
+        //decorateur d'element
+        $decoratorsBis = array(
+            'ViewHelper',
+            'Errors',
+            array('Label', array('tag' => '<br>', 'class' => 'label')),
+            array('HtmlTag', array('tag' => 'li'))
+        );
 
-		// on insere le decorateurde form au formulaire
-		$this->setDecorators($decoratorsForm);
+        //decorateur de formulaire
+        $decoratorsForm = array(
+            'FormElements',
+            array('Fieldset', array('tag' => 'div')),
+            array('HtmlTag', array('tag' => 'ul')),
+            array(
+                array('DivTag' => 'HtmlTag'),
+                array('tag' => 'div')
+            ),
+            'Form'
+        );
 
-	//===============Creation des element
+        // on insere le decorateurde form au formulaire
+        $this->setDecorators($decoratorsForm);
 
-		$eAeroportDepart = new Zend_Form_Element_Select('aeroportDepart');
-		$eAeroportDepart->setLabel('De')
-						->setRequired(true)
-						->addValidator('notEmpty')
-						->setMultiOptions($this->listVille())
-						->setDecorators($decorators);
+    //===============Creation des element
 
-		$eAeroportArrivee = new Zend_Form_Element_Select('aeroportArrive');
-		$eAeroportArrivee->setLabel('A')
-						 ->setRequired(true)
-						 ->addValidator('notEmpty')
-						 ->setMultiOptions($this->listVille())
-						 ->setDecorators($decorators);
+        $eAeroportDepart = new Zend_Form_Element_Select('aeroportDepart');
+        $eAeroportDepart->setLabel('De')
+                        ->setRequired(true)
+                        ->addValidator('notEmpty')
+                        ->setMultiOptions($this->listVille())
+                        ->setDecorators($decorators);
 
-		$eTypeTrajet = new Zend_Form_Element_Radio('typeTrajet');
-		$eTypeTrajet->setMultiOptions(array('1'=>'Aller simple','2'=>'Aller retour')  )
-        			->setOptions(array('separator'=>' '))
-        			->setValue('2')
-        			->setDecorators($decorators);
+        $eAeroportArrivee = new Zend_Form_Element_Select('aeroportArrive');
+        $eAeroportArrivee->setLabel('À')
+                         ->setRequired(true)
+                         ->addValidator('notEmpty')
+                         ->setMultiOptions($this->listVille())
+                         ->setDecorators($decorators);
+
+        $eTypeTrajet = new Zend_Form_Element_Radio('typeTrajet');
+        $eTypeTrajet->setMultiOptions(array( '2'=>'Aller retour'
+                                            ,'1'=>'Aller simple'))
+                    ->setOptions(array('separator'=>' '))
+                    ->setValue('2')
+                    ->setDecorators($decorators);
 
         $eDatedeb = new Zend_Form_Element_Text('datepickerdeb');
-		$eDatedeb	->setLabel('Date aller')
-					->setRequired(true)
-					->setDecorators($decorators);
+        $eDatedeb   ->setLabel('Date aller')
+                    ->setRequired(true)
+                    ->setAttrib('size', '7')
+                    ->setDecorators($decoratorsBis);
 
-		$eDatefin = new Zend_Form_Element_Text('datepickerfin');
-		$eDatefin	->setLabel('Date retour')
-					->setDecorators($decorators);
+        $eDatefin = new Zend_Form_Element_Text('datepickerfin');
+        $eDatefin   ->setLabel('Date retour')
+                    ->setAttrib('size', '7')
+                    ->setDecorators($decoratorsBis);
 
-		$nbr=array();
-		for($i=1;$i<10;$i++)
-			$nbr[$i]=$i;
+        $nbr=array();
+        for($i=1;$i<10;$i++)
+            $nbr[$i]=$i;
 
-		$eNbrPassager = new Zend_Form_Element_Select('nbrPassager');
-		$eNbrPassager	->setLabel('Nombre de passagers : ')
-						->setRequired(true)
-						->addValidator('notEmpty')
-						->setMultiOptions($nbr)
-						->setDecorators($decorators);
+        $eNbrPassager = new Zend_Form_Element_Select('nbrPassager');
+        $eNbrPassager   ->setLabel('Nombre de passagers : ')
+                        ->setRequired(true)
+                        ->addValidator('notEmpty')
+                        ->setMultiOptions($nbr)
+                        ->setDecorators($decoratorsBis);
 
-		$typePassager = array('1' => 'Adultes (25-64 ans)','0.5' => 'Enfants (2-11 ans)','0.75' => 'Séniors (65 ans et plus)');
+        $typePassager = array( '1' => 'Adultes (25-64 ans)'
+                              ,'0.5' => 'Enfants (2-11 ans)'
+                              ,'0.75' => 'Séniors (65 ans et plus)');
 
-		$eTypePassager = new Zend_Form_Element_Select('typePassager');
-		$eTypePassager	->setLabel('Passager N° 1 : ')
-						->setMultiOptions($typePassager)
-						->setDecorators($decorators);
+        $eTypePassagerA = new Zend_Form_Element_Select('typePassager');
+        $eTypePassagerA ->setLabel('1 : ')
+                        ->setMultiOptions($typePassager)
+                        ->setDecorators($decorators);
 
-		$eTypePassager2 = new Zend_Form_Element_Select('typePassager2');
-		$eTypePassager2	->setMultiOptions($typePassager)
-						->setLabel('Passager N° 2 : ')
-						->setDecorators($decorators);
+        $eTypePassagerB = new Zend_Form_Element_Select('typePassager2');
+        $eTypePassagerB ->setMultiOptions($typePassager)
+                        ->setLabel('2 : ')
+                        ->setDecorators($decorators);
 
-		$eTypePassager3 = new Zend_Form_Element_Select('typePassager3');
-		$eTypePassager3	->setMultiOptions($typePassager)
-						->setLabel('Passager N° 3 : ')
-						->setDecorators($decorators);
+        $eTypePassagerC = new Zend_Form_Element_Select('typePassager3');
+        $eTypePassagerC ->setMultiOptions($typePassager)
+                        ->setLabel('3 : ')
+                        ->setDecorators($decorators);
 
-		$eTypePassager4 = new Zend_Form_Element_Select('typePassager4');
-		$eTypePassager4	->setMultiOptions($typePassager)
-						->setLabel('Passager N° 4 : ')
-						->setDecorators($decorators);
+        $eTypePassagerD = new Zend_Form_Element_Select('typePassager4');
+        $eTypePassagerD ->setMultiOptions($typePassager)
+                        ->setLabel('4 : ')
+                        ->setDecorators($decorators);
 
-		$eTypePassager5 = new Zend_Form_Element_Select('typePassager5');
-		$eTypePassager5	->setMultiOptions($typePassager)
-						->setLabel('Passager N° 5 : ')
-						->setDecorators($decorators);
+        $eTypePassagerE = new Zend_Form_Element_Select('typePassager5');
+        $eTypePassagerE ->setMultiOptions($typePassager)
+                        ->setLabel('5 : ')
+                        ->setDecorators($decorators);
 
-		$eTypePassager6 = new Zend_Form_Element_Select('typePassager6');
-		$eTypePassager6	->setMultiOptions($typePassager)
-						->setLabel('Passager N° 6 : ')
-						->setDecorators($decorators);
+        $eTypePassagerF = new Zend_Form_Element_Select('typePassager6');
+        $eTypePassagerF ->setMultiOptions($typePassager)
+                        ->setLabel('6 : ')
+                        ->setDecorators($decorators);
 
-		$eTypePassager7 = new Zend_Form_Element_Select('typePassager7');
-		$eTypePassager7	->setMultiOptions($typePassager)
-						->setLabel('Passager N° 7 : ')
-						->setDecorators($decorators);
+        $eTypePassagerG = new Zend_Form_Element_Select('typePassager7');
+        $eTypePassagerG ->setMultiOptions($typePassager)
+                        ->setLabel('7 : ')
+                        ->setDecorators($decorators);
 
-		$eTypePassager8 = new Zend_Form_Element_Select('typePassager8');
-		$eTypePassager8	->setMultiOptions($typePassager)
-						->setLabel('Passager N° 8 : ')
-						->setDecorators($decorators);
+        $eTypePassagerH = new Zend_Form_Element_Select('typePassager8');
+        $eTypePassagerH ->setMultiOptions($typePassager)
+                        ->setLabel('8 : ')
+                        ->setDecorators($decorators);
 
-		$eTypePassager9 = new Zend_Form_Element_Select('typePassager9');
-		$eTypePassager9	->setMultiOptions($typePassager)
-						->setLabel('Passager N° 9 : ')
-						->setDecorators($decorators);
+        $eTypePassagerI = new Zend_Form_Element_Select('typePassager9');
+        $eTypePassagerI ->setMultiOptions($typePassager)
+                        ->setLabel('9 : ')
+                        ->setDecorators($decorators);
 
-		$eTypePassager10 = new Zend_Form_Element_Select('typePassager10');
-		$eTypePassager10->setMultiOptions($typePassager)
-						->setLabel('Passager N° 10 : ')
-						->setDecorators($decorators);
+        $eTypePassagerJ = new Zend_Form_Element_Select('typePassager10');
+        $eTypePassagerJ ->setMultiOptions($typePassager)
+                        ->setLabel('10 : ')
+                        ->setDecorators($decorators);
 
-		$classe = array('1'=>'Economique','2.5'=>'Première','2'=>'Affaire');
+        $classe = array( '1'=>'Economique'
+                        ,'2.5'=>'Première'
+                        ,'2'=>'Affaire');
 
-		$eClasse = new Zend_Form_Element_Select('classe');
-		$eClasse	->setLabel('Classe : ')
-					->setRequired(true)
-					->addValidator('notEmpty')
-					->setMultiOptions($classe)
-					->setDecorators($decorators);
+        $eClasse = new Zend_Form_Element_Select('classe');
+        $eClasse    ->setLabel('Classe : ')
+                    ->setRequired(true)
+                    ->addValidator('notEmpty')
+                    ->setMultiOptions($classe)
+                    ->setDecorators($decoratorsBis);
 
-		$eSubmit = new Zend_Form_Element_Submit('reserver');
-		$eSubmit 	->setAttrib('id', 'SBTReserver')
-					->setAttrib('class', 'bgRedBtn')
-					->setLabel('Réserver');
+        $eSubmit = new Zend_Form_Element_Submit('reserver');
+        $eSubmit     ->setAttrib('id', 'SBTReserver')
+                    ->setAttrib('class', 'bgRedBtn')
+                    ->setLabel('Réserver');
 
-		$elements = array($eAeroportDepart, $eAeroportArrivee, $eTypeTrajet, $eDatedeb, $eDatefin, $eNbrPassager, $eTypePassager, $eTypePassager2, $eTypePassager3, $eTypePassager4, $eTypePassager5, $eTypePassager6, $eTypePassager7, $eTypePassager8, $eTypePassager9, $eTypePassager10, $eClasse, $eSubmit);
-		$this->addElements($elements);
+        $elements = array($eAeroportDepart
+                        , $eAeroportArrivee
+                        , $eTypeTrajet
+                        , $eDatedeb
+                        , $eDatefin
+                        , $eNbrPassager
+                        , $eTypePassagerA
+                        , $eTypePassagerB
+                        , $eTypePassagerC
+                        , $eTypePassagerD
+                        , $eTypePassagerE
+                        , $eTypePassagerF
+                        , $eTypePassagerG
+                        , $eTypePassagerH
+                        , $eTypePassagerI
+                        , $eTypePassagerJ
+                        , $eClasse
+                        , $eSubmit
+                        );
+        $this->addElements($elements);
 
+    //=============== creation des groupes de formulaire
+        $this->addDisplayGroup(array('aeroportDepart','aeroportArrive'),
+                                'un',
+                                array("legend" => ""));
+        
+        $this->addDisplayGroup(array('typeTrajet'),
+                                'deux',
+                                array("legend" => ""));
 
-	//=============== creation des groupes de formulaire
-		$this->addDisplayGroup(array(
-								'aeroportDepart',
-								'aeroportArrive'), 'un', array("legend" => ""));
-		$this->addDisplayGroup(array(
-								'typeTrajet'), 'deux', array("legend" => ""));
-		$this->addDisplayGroup(array(
-								'datepickerdeb',
-								'datepickerfin'), 'trois', array("legend" => ""));
-		$this->addDisplayGroup(array(
-								'nbrPassager',
-								'typePassager',
-								'typePassager2',
-								'typePassager3',
-								'typePassager4',
-								'typePassager5',
-								'typePassager6',
-								'typePassager7',
-								'typePassager8',
-								'typePassager9',
-								'typePassager10',
-								), 'quatre', array("legend" => ""));
-		$this->addDisplayGroup(array(
-								'classe'), 'cinq', array("legend" => ""));
-		$this->addDisplayGroup(array(
-								'reserver'), 'six', array("legend" => ""));
+        $this->addDisplayGroup(array('datepickerdeb','datepickerfin'),
+                                'trois',
+                                array("legend" => ""));
 
-	}
+        $this->addDisplayGroup(array('nbrPassager'),
+                                'quatre',
+                                array("legend" => ""));
 
-	/**
-     * Liste des Pays
-     */
+        $this->addDisplayGroup(array('typePassager',
+                                    'typePassager2',
+                                    'typePassager3',
+                                    'typePassager4',
+                                    'typePassager5',
+                                    'typePassager6',
+                                    'typePassager7',
+                                    'typePassager8',
+                                    'typePassager9',
+                                    'typePassager10'),
+                                'quatreBis',
+                                array("legend" => ""));
+
+        $this->addDisplayGroup(array('classe'),
+                                'cinq',
+                                array("legend" => ""));
+        $this->addDisplayGroup(array('reserver'),
+                                'six',
+                                array("legend" => ""));
+
+    }
+
+    /**
+    * Liste des villes
+    */
     private function listVille () {
-		// on charge le model
-		$tableVille = new TVille;
-		// on recupere tout les pays
-        $reqVille = $tableVille	->select()
-    							->order("nom_ville");
+        // on charge les models
+        $tableAeroport = new TAeroport;
+ 
+        // on recupere tout les pays
+        $reqAeroport = $tableAeroport   ->select()
+                                        ->from($tableAeroport)
+                                        ->order("nom_aeroport");
 
-	    $ville = $tableVille->fetchAll($reqVille);
+        $aeroport = $tableAeroport->fetchAll($reqAeroport);
 
-        // on instancie le resultat en tableau de ville
-        $villeTab = array();
+        // on instancie le resultat en tableau de pays
+        $aeroportTab = array();
 
-        $villeTab["-1"] = "-- Choisissez --"; 
-        foreach ($ville as $v) {
-        	$villeTab[$v->id_ville] = utf8_encode($v->nom_ville);
+        $aeroportTab[""] = "-- Choisissez --"; 
+        foreach ($aeroport as $a) {
+            $tableAeroport = new TAeroport;
+            // on recherche l'aeroport par clé primaire
+            $aeroport = $tableAeroport  ->find($a->trigramme)
+                                        ->current();
+            $ville = $aeroport->findParentRow('TVille');
+
+            $aeroportTab[$ville->id_ville] = utf8_encode($a->nom_aeroport . '(' . $a->trigramme . ')' . ', ' . $ville->nom_ville);
         }
  
-        return $villeTab;
+        return $aeroportTab;
     }
 }
