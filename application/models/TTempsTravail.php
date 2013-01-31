aaf1d7f455f0ea66d8c6a3a485490791736467e1
victormalfait <victor.malfait@gmail.com>
2013-01-31 21:31:46 +0100

    MAJ

--- application/models/TTempsTravail.php
+++ application/models/TTempsTravail.php
@@ -9,13 +9,6 @@ class TTempsTravail extends Zend_Db_Table_Abstract
 	protected $_referenceMap = array(
 		"Pilote" => array(
 			"columns" => "id_pilote",
-			"columns" => "id_copilote",
-			"refTableClass" => "TPilote"),
-		"Avion" => array(
-			"columns" => "immatriculation",
-			"refTableClass" => "TAvion"),
-		"Destination" => array(
-			"columns" => "id_destination",
-			"refTableClass" => "TDestination")
+			"refTableClass" => "TPilote")
 		);
 }
